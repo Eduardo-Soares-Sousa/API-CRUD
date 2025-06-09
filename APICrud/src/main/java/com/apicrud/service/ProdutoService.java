707987797d3eb99f7096b5b3f8b9537e33fbdfae
Eduardo Soares <eduardo_soares001@outlook.com>
2025-06-09 15:28:57 -0300

    Implementação do service

--- APICrud/src/main/java/com/apicrud/service/ProdutoService.java
+++ APICrud/src/main/java/com/apicrud/service/ProdutoService.java
@@ -1,2 +1,31 @@
-package com.apicrud.service;public class ProdutoService {
+package com.apicrud.service;
+
+import com.apicrud.model.Produto;
+import com.apicrud.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProdutoService {
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public List<Produto> listar() {
+        return produtoRepository.findAll();
+    }
+
+    public Produto salvar(Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    public Optional<Produto> buscar(Long id) {
+        return produtoRepository.findById(id);
+    }
+
+    public void deletar(Long id) {
+        produtoRepository.deleteById(id);
+    }
 }
